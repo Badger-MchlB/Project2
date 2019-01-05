0115dc85866722e2f8ef627b75d6b721e3b28ee0
duda <mariaecabral@outlook.com>
2019-01-05 11:02:44 -0500

    commit

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const express = require("express");
-const PORT = process.env.PORT || 8080;
-const app = express();
+var express = require("express")
+var bodyParser = require("body-parser")
 
-app.listen(PORT, () => {
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+var PORT = process.env.PORT || 8080
+
+var app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+var exphbs = require("express-handlebars")
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+var routes = require("./controllers/controller.js")
+
+app.use(routes)
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT)
+})
